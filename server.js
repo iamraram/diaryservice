84931897cecf3cb17c8d3ebf7a34a706d9308c65
iamraram <slyram06@icloud.com>
2022-02-06 02:34:08 +0900

    마이페이지, 상세페이지 수정

--- server.js
+++ server.js
@@ -40,20 +40,36 @@ app.get('/signup', islogin2, function(req, res){
 });
 
 app.post('/signup', function (req, res) {
-    if ((req.body.id).length < 6 || (req.body.pw).length < 6) {
-        res.render('resignup.ejs')
+    if (((req.body.id).length < 6 || (req.body.pw).length < 6)){
+        res.render('resignup.ejs', {
+            err: '양식에 맞춰 가입해주세요.'
+        })
     }
-    db.collection('user').findOne(
-        {
-          id: req.body.id
-        }, 
-      
-      function(err, result) {
+    else if ((req.body.age < 1922) || (req.body.age > 2022)){
+        res.render('resignup.ejs', {
+            err: '올바른 나이를 입력해주세요.'
+        })
+    }
+    else if (req.body.pw != req.body.pw2) {
+        res.render('resignup.ejs', {
+            err: '비밀번호가 일치하지 않습니다.'
+        })
+    }
+    else {
+        db.collection('user').findOne(
+            {
+                id: req.body.id
+            }, 
+        
+        function(err, result) {
             if (!result) {
                 db.collection('user').insertOne(
                     {
-                      id: req.body.id,
-                      pw: req.body.pw
+                        id: req.body.id,
+                        pw: req.body.pw,
+                        name: req.body.name,
+                        school: req.body.school,
+                        age: req.body.age
                     },
             
                     function (err, result) {
@@ -64,7 +80,9 @@ app.post('/signup', function (req, res) {
             else {
                 res.render('signupfail.ejs')
             }
-      })
+        })
+
+    }
 });
 
 app.get('/', islogin2, function(req, res){
@@ -95,9 +113,20 @@ app.get('/logout', function(req, res){
     res.render('logout.ejs');
 });
 
-app.get('/mypage', islogin, function(req, res){
-    console.log(req.user)
-    res.render('mypage.ejs')
+app.get('/mypage', islogin, function (req, res) {
+    db.collection('user').findOne(function(err, result) {
+        const name = result.name;
+        const id = result.id;
+        const age = result.age;
+        const school = result.school;
+
+        res.render('mypage.ejs', {
+            name: name,
+            id: id,
+            age: age,
+            school: school
+        });
+    });
 });
 
 function islogin(req, res, next) {
@@ -108,7 +137,7 @@ function islogin(req, res, next) {
         res.render('404.ejs')
     }
 }
-  
+
 passport.use(new LocalStrategy({
     usernameField: 'id',
     passwordField: 'pw',
@@ -119,13 +148,19 @@ passport.use(new LocalStrategy({
     console.log(input_id, input_pw);
 
     db.collection('user').findOne({ id: input_id }, function (err, result) {
-      if (err) return done(에러)
-  
-      if (!result) return done(null, false, { message: '존재하지 않는 아이디입니다.' })
+      if (err) {
+          return done(에러)
+      }
+      if (!result) return done(null, false, {
+          message: '존재하지 않는 아이디입니다.'
+        })
       if (input_pw == result.pw) {
         return done(null, result)
-      } else {
-        return done(null, false, { message: '비밀번호가 틀렸습니다.' })
+      }
+      else {
+        return done(null, false, {
+            message: '비밀번호가 틀렸습니다.'
+        })
       }
     })
 }));
@@ -135,7 +170,13 @@ passport.serializeUser(function (user, done) {
 });
   
 passport.deserializeUser(function (input_id, done) {
-    db.collection('user').findOne({ id: input_id }, function (에러, 결과) {
+    db.collection('user').findOne({ id: input_id },
+    function (에러, 결과) {
       done(null, 결과)
     })
 }); 
+
+app.get('/:id', islogin2, function(req, res){
+    res.render('unknown.ejs')
+});
+  
